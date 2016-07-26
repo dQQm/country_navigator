9ed4f475099b6379cc6408e82e3ed7e7451a0505
Mariya Dzhingova <mdzhitc@training.local>
2016-07-26 20:14:54 +0300

    service fix

---
+++ app/services/countries.js
@@ -0,0 +1,50 @@
+(function (angular) {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('countriesInfo', countriesInfo);
+
+    countriesInfo.$inject = ['$http'];
+
+    function countriesInfo($http) {
+        return {
+            data:[{
+                "countrieName": "test",
+                "capitalName": "testName",
+                "flagUrl": "www.example.com",
+                "language": "testLanguage",
+                "currency": "gold",
+                "population": "10000",
+                "size": "10000 10000",
+                "description": "so many descriptions",
+                "recomendations": ["stuf", "one"]
+            }, {
+                "countrieName": "test",
+                "capitalName": "testName",
+                "flagUrl": "www.example.com",
+                "language": "testLanguage",
+                "currency": "gold",
+                "population": "10000",
+                "size": "10000 10000",
+                "description": "so many descriptions",
+                "recomendations": ["stuf", "one"]
+            }, {
+                "countrieName": "test",
+                "capitalName": "testName",
+                "flagUrl": "www.example.com",
+                "language": "testLanguage",
+                "currency": "gold",
+                "population": "10000",
+                "size": "10000 10000",
+                "description": "so many descriptions",
+                "recomendations": ["stuf", "one"]
+            }]
+        };
+    }
+
+
+
+
+
+}(angular));
