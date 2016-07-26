075c5449114523750ba64ff797697fac5d2e6674
rayabakalova <rbakalova7@gmail.com>
2016-07-26 20:24:06 +0300

    start form

---
+++ app/states/country-edit/form.js
@@ -0,0 +1,35 @@
+(function (angular) {
+    'use strict';
+
+    angular
+        .module('app')
+        .directive('form', form)
+        .config(config);
+
+    config.$inject = ['$stateProvider'];
+    function config($stateProvider) {
+        $stateProvider
+            .state('form', {
+                url: '/form',
+                template: '<form-state></form-state>'
+            });
+    }
+
+    function hi() {
+        var directive = {
+            templateUrl: './states/form/form.html',
+            restrict: 'E',
+            controller: controller,
+            scope: {
+            }
+        };
+
+        return directive;
+    }
+
+    controller.$inject = ['$scope', 'survey'];
+    function controller($scope, survey) {
+      
+    }
+
+}(angular));
