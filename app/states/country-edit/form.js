1fab0134aa68a6effb0af2db07a99ef329016713
rayabakalova <rbakalova7@gmail.com>
2016-08-08 20:56:49 +0300

    Form update

--- app/states/country-edit/form.js
+++ app/states/country-edit/form.js
@@ -3,7 +3,7 @@
 
     angular
         .module('app')
-        .directive('form', form)
+        .directive('formState', form)
         .config(config);
 
     config.$inject = ['$stateProvider'];
@@ -15,12 +15,13 @@
             });
     }
 
-    function hi() {
+    function form() {
         var directive = {
-            templateUrl: './states/form/form.html',
+            templateUrl: './states/country-edit/form.html',
             restrict: 'E',
             controller: controller,
             scope: {
+                name: '='
             }
         };
 
@@ -29,7 +30,33 @@
 
     controller.$inject = ['$scope', 'survey'];
     function controller($scope, survey) {
-      
+        $scope.survey = survey;
+
+        $scope.sayHi = sayHi;
+        $scope.sayHi1 = sayHi1;
+        $scope.sayHi2 = sayHi2;
+
+        function sayHi() {
+            alert('Hi to you ' + $scope.name + '! Give me ' + survey.getTotalNumber() + '!');
+        }
+
+        function sayHi1() {
+            return 'Hi Maria!';
+        }
+
+        function sayHi2() {
+            //ask the user for a string
+          prompt({
+            title: 'Give me a name',
+            message: 'What would you like to name it?',
+            input: true,
+            label: 'Name',
+            value: ''
+          }).then(function(name){
+            //the promise is resolved with the user input
+            console.log('and the name is ', name);
+          }); 
+        }
     }
 
 }(angular));
