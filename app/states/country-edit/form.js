81965faae0b4bcc9d448148d3b7ae94b7bdd71d6
rayabakalova <rbakalova7@gmail.com>
2016-08-14 21:57:52 +0300

    Validation updates

--- app/states/country-edit/form.js
+++ app/states/country-edit/form.js
@@ -30,33 +30,6 @@
 
     controller.$inject = ['$scope', 'survey'];
     function controller($scope, survey) {
-        $scope.survey = survey;
-
-        $scope.sayHi = sayHi;
-        $scope.sayHi1 = sayHi1;
-        $scope.sayHi2 = sayHi2;
-
-        function sayHi() {
-            alert('Hi to you ' + $scope.name + '! Give me ' + survey.getTotalNumber() + '!');
-        }
-
-        function sayHi1() {
-            return 'Hi Maria!';
-        }
-
-        function sayHi2() {
-            //ask the user for a string
-          prompt({
-            title: 'Give me a name',
-            message: 'What would you like to name it?',
-            input: true,
-            label: 'Name',
-            value: ''
-          }).then(function(name){
-            //the promise is resolved with the user input
-            console.log('and the name is ', name);
-          }); 
-        }
-    }
+       }
 
 }(angular));
