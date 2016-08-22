eae44022f0158c1c92313cb46b3036596b81a15e
Unicorn <mariazhingova@gmail.com>
2016-08-22 21:33:41 +0300

    ui modification

--- app/services/countries.js
+++ app/services/countries.js
@@ -22,17 +22,17 @@
                 "description": "so many descriptions",
                 "recomendations": ["stuf", "one"]
             }, {
-                "countrieName": "Samsung",
-                "capitalName": "testName",
-                "flagUrl": "http://seeklogo.com/images/S/s-h-i-e-l-d-logo-F89847BD30-seeklogo.com.gif",
-                "language": "testLanguage",
-                "currency": "gold",
-                "population": "10000",
-                "size": "10000 10000",
+                "countrieName": "Austria",
+                "capitalName": "Vienna",
+                "flagUrl": "http://flagpedia.net/data/flags/big/at.png",
+                "language": "German",
+                "currency": "Euro",
+                "population": 8725931,
+                "size": 83879,
                 "lat":40.7127837,
                 "lan":-74.00594130000002,
-                "description": "so many descriptions",
-                "recomendations": ["stuf", "one"]
+                "description": "Officially the Republic of Austria is a federal republic and a landlocked country of over 8.66 million people in Central Europe. It is bordered by the Czech Republic and Germany to the north, Hungary and Slovakia to the east, Slovenia and Italy to the south, and Switzerland and Liechtenstein to the west. The territory of Austria covers 83,879 square kilometres (32,386 sq mi). Austria's terrain is highly mountainous, lying within the Alps; only 32% of the country is below 500 metres (1,640 ft), and its highest point is 3,798 metres (12,461 ft). The majority of the population speak local Bavarian dialects of German as their native language, and Austrian German in its standard form is the country's official language.Other local official languages are Hungarian, Burgenland Croatian, and Slovene.",
+                "recomendations": ["stuf", "one"],
             }, {
                 "countrieName": "test",
                 "capitalName": "testName",
